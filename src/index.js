e1b99839ed5415edce66f340fc74da68d448c85d
Nadeem Ahmad <nadeem2136@gmail.com>
2023-11-23 14:09:02 +0530

    utils code discussion

--- src/index.js
+++ src/index.js
@@ -4,7 +4,19 @@ import connectDB from "./db/index.js";
 
 dotenv.config({ path: "./env" });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("ERROR: ", error);
+      throw error;
+    });
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at port: ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
+  });
 
 /*
 import express from "express";
